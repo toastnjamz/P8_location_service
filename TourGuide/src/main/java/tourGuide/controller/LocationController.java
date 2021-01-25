53f85b929a152f185110751f692a51b545df9d88
toastnjamz <veritydavis@gmail.com>
2021-01-24 19:35:05 -0700

    Adding OpenSwagger and JavaDocs

--- TourGuide/src/main/java/tourGuide/controller/LocationController.java
+++ TourGuide/src/main/java/tourGuide/controller/LocationController.java
@@ -1,6 +1,7 @@
 package tourGuide.controller;
 
 import gpsUtil.location.VisitedLocation;
+import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,18 @@ import tourGuide.service.LocationService;
 import java.util.UUID;
 
 @RestController
+@Api(description="Location microservice for TourGuide")
 public class LocationController {
 
     @Autowired
     private LocationService locationService;
     private final Logger logger = LoggerFactory.getLogger(LocationController.class);
 
+    /**
+     * Gets a user's last visited location
+     * @param userId
+     * @return visited location
+     */
     @RequestMapping("/user-location")
     public VisitedLocation getUserLocation(@RequestParam String userId) {
         VisitedLocation visitedLocation = locationService.getUserLocation(UUID.fromString(userId));
@@ -26,6 +33,10 @@ public class LocationController {
         return visitedLocation;
     }
 
+    /**
+     * Gets a list of all attractions
+     * @return list of all attractions
+     */
     @RequestMapping("/attractions")
     public AttractionListWrapper getAttractions() {
         AttractionListWrapper attractionListWrapper = new AttractionListWrapper();
